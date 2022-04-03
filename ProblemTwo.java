9560bee55d9ba1170473e2cb9fff014f0ef32c88
Faint <73570576+faintsign@users.noreply.github.com>
2022-04-03 10:20:46 -0400

    Add files via upload

---
+++ ProblemTwo.java
@@ -0,0 +1,43 @@
+/*
+    file: problemTwo
+    project: Euler
+    Date: 03/30/22
+    Author: FaintSign
+    
+    Description:
+    Calculate the sum of the even fibonacii sum
+*/ 
+
+import java.util.LinkedList;
+public class problemTwo {
+    
+    public static int fibonaciiSum(int N){
+        int sum = 0; // -> The number of even numbers
+        int firstTerm = 0; // -> The first term starting at 0
+        int secondTerm = 1; // -> The second term starting at 1
+
+        while(firstTerm <= N ){
+            
+            int fibonaciiTerm = firstTerm + secondTerm; // -> The sum of the first and second terms
+
+            // The first term will be equal to the second term
+            // The second term will now be equal to the fibonacii term
+            firstTerm = secondTerm;
+            secondTerm = fibonaciiTerm;
+
+            if(fibonaciiTerm % 2 == 0){
+                sum+= fibonaciiTerm;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args){
+
+        final int MAX_NUMBER = 4000000;
+        System.out.println(fibonaciiSum(MAX_NUMBER));
+
+    }
+    
+}
